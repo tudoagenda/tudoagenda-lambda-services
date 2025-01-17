8da82a650d3d5e1091af8ce8dd0a38a651f9d1b9
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-17 20:28:08 -0300

    chore: bump version

--- services/whatsapp.service.ts
+++ services/whatsapp.service.ts
@@ -3,12 +3,15 @@ import axios from "axios";
 export const WhatsAppService = {
   sendMessage: async (to: string, message: string) => {
     await axios.post(
-      `https://graph.facebook.com/v18.0/${process.env.WHATSAPP_NUMBER_ID}/messages`,
+      `https://graph.facebook.com/v21.0/${process.env.WHATSAPP_NUMBER_ID}/messages`,
       {
         messaging_product: "whatsapp",
         to: "5514996978287",
         type: "template",
-        template: { name: "hello_world", language: { code: "en_US" } },
+        template: {
+          name: "hello_world",
+          language: { code: "en_US" },
+        },
       },
       {
         headers: {
