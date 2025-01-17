0a603b8018d8cb7d4951b7f29102937b6e94e0db
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-01-17 20:18:21 -0300

    fix: fix whatsapp id

--- services/whatsapp.service.ts
+++ services/whatsapp.service.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const WhatsAppService = {
   sendMessage: async (to: string, message: string) => {
     await axios.post(
-      `https://graph.facebook.com/v18.0/${process.env.WHATSAPP_PHONE_NUMBER_ID}/messages`,
+      `https://graph.facebook.com/v18.0/${process.env.WHATSAPP_NUMBER_ID}/messages`,
       {
         messaging_product: "whatsapp",
         to: to,
