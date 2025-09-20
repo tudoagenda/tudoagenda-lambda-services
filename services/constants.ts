ff032ed483d001ed5c846e6a0cb0bfd16837b045
Carlos Nardo <c.cesar.nardo@gmail.com>
2025-09-20 15:27:24 -0300

    change ec2 url

--- services/constants.ts
+++ services/constants.ts
@@ -1,5 +1,5 @@
 export const WAHA_API_BASE_URL =
-  "http://ec2-98-81-185-133.compute-1.amazonaws.com:3000";
+  "http://ec2-54-208-244-124.compute-1.amazonaws.com:3000";
 
 export const WAHA_API_URL = {
   chatting: {
